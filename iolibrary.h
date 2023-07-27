f05f4bca619811c29e8293f11e01826f14315805
Shane Feek <shane.feek@structuretec.local>
2023-07-27 11:39:11 -0400

    Major refactor
    Signed-off-by: Shane Feek <shane.feek@structuretec.local>

--- iolibrary.h
+++
@@ -1,5 +0,0 @@
-int stringtodouble (const char *str, double *v);
-int stringtoint (const char *str, int *v);
-double getdouble (const char *display);
-int getint (const char *display);
-char *getstring (const char *display);
